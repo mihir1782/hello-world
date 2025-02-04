426edc0dc29231448afecdc0f1136864284d3c50
[mihir1782] <[mjoshi1782@gmail.com]>
2025-02-03 19:23:28 -0500

    31:28 Views and URL's

---
+++ studybud/studybud/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('Home page')
+
+def room(request):    
+    return HttpResponse("Room")
+    
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home),
+    path('room/',room)
+]
+
